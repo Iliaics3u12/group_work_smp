4f5a948b62e531895ca742ebae8c9c0f2a65066d
Iliaics3u12 <iliarezaei.doc.sch@gmail.com>
2023-12-19 10:04:01 -0500

    The group assignmnet due Dec 19th.

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("anything")
-print("Nothing")
-print("Everything")
+
+#Ilia (Part 3)
+
+def main():
+    while True:
+        print("What task would you like to use:")
+        print("1: Finding the factorial")
+        print("2: Finding the fibonacci value")
+        user_selection = int(input("Enter the number to be performed: "))
+
+        if user_selection == 1:
+            num = int(input("Enter the integer you'd like to find its factorial: "))
+            print(f"The factorial of {num} is: {factorial(num)}")
+        elif user_selection == 2:
+            position_sequ = int(input("Enter the sequence position of the fibonacci value: "))
+            print(f"The {position_sequ}th fibonacci is {fibonacci(position_sequ)}")
+        else:
+            print("Pleaser select 1 or 2.")
+
+
+if __name__ == "__main__":
+    main()
