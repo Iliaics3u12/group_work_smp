d0a6e6a45b509c97156fe917c225a824f1e9a330
Mahmoudhod <mahmoudhod4@gmail.com>
2023-12-19 18:06:00 +0200

    hhh

--- fibonacci.py
+++ fibonacci.py
@@ -3,8 +3,11 @@ def fibonacci(n):
         return 0
     elif n == 1:
         return 1
+    elif n == 2:
+        return 2
     else:
-        a, b = 0, 1
+        a, b = 1, 2
         for _ in range(2, n + 1):
             a, b = b, a + b
-        return b
+        return a
+
